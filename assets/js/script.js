acb73abaa34f299795977813e95b3161c92efb3d
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-04-16 12:58:19 -0500

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,15 +77,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     console.log(event);
   },
   update: function() {
@@ -128,12 +134,14 @@ tolerance: "touch",
 drop: function(event, ui) {
   // remove dragged element from the dom
   ui.draggable.remove();
-
+  $(".bottom-trash").removeClass(".bottom-trash-active");
 },
 over: function(event, ui) {
   console.log(ui);
+  $(".bottom-trash").addClass("bottom-trash-active");
 },
 out: function(event, ui) {
+  $(".bottom-trash").removeClass("bottom-trash-active");
   console.log(ui);
 }
 });
@@ -157,7 +165,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -296,3 +304,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function() {
+    auditTask($(this));
+  });
+}, 1800000);
